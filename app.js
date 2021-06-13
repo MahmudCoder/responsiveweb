01c1d09de8203fea3ba3c0a4a53d56d6caa7d4d1
Mahmudul Hasan Mamun <84878766+MahmudCoder@users.noreply.github.com>
2021-06-13 10:13:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+    const showHideBtn = document.querySelector('#menuBarIcon')
+
+
+showHideBtn.addEventListener('click',function(){
+
+    const showBar = document.querySelector('.navbarMenu');
+showBar.classList.toggle('showMenu')
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
